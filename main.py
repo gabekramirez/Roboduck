5b5b7e66950620ad839c2602770474597947b58a
Gabriel Ramirez <gabekramirez@gmail.com>
2026-04-17 15:53:54 -0700

    removed unused function

--- main.py
+++ main.py
@@ -7,12 +7,6 @@ import os
 import sys
 
 
-def resource_path(relative_path):
-    if hasattr(sys, '_MEIPASS'):
-        return os.path.join(sys._MEIPASS, relative_path)
-    return os.path.join(os.path.abspath("."), relative_path)
-
-
 ENABLE_CONTROLLERS = True
 IS_WEB = False
 IS_MOBILE = False
